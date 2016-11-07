a69073636f2ce63b3ad875785889f74ef14c5c7b
Oren Lahav <orenlahav@yahoo.com>
2016-11-07 16:18:51 -0500

    First commit

---
+++ game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	private Rigidbody2D rb2d;
+	public float speed;
+	private AudioSource audio;
+	private bool isFacingRight;
+	private Animator animator;
+
+
+	void Awake() {
+		//LoadPlayerPos (0, -1f, 0);
+		audio = GetComponent<AudioSource>();
+	}
+
+    // Use this for initialization
+    void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		isFacingRight = true;
+		speed = 3.0f;
+		//LoadPlayerPos (0, 2, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal * speed, moveVertical * speed);
+		MovePlayer (movement);
+		if ((moveHorizontal < 0 && isFacingRight) || (moveHorizontal > 0 && !isFacingRight))
+			Flip ();
+		//make footsteps sound when player moves
+		if (Input.GetButtonDown ("Horizontal") || Input.GetButtonDown ("Vertical")) {
+			
+			audio.Play ();
+		} 
+		// stop footsteps soundswhen player stops moving
+		else if (Input.GetButtonUp ("Vertical")) {
+			
+			audio.Stop ();
+		}
+    }
+
+	void MovePlayer(Vector2 direction) {	
+		// Get the player's current position
+		Vector2 pos = transform.position;
+
+		// Calculate the new position
+		pos += direction * speed * Time.deltaTime;
+
+		// Update the player's position
+		transform.position = pos;
+	}
+
+
+	// Destroy the player if the enemy collides with it
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.CompareTag ("Enemy")) {
+			animator.SetBool("hit", true);
+
+			//other.gameObject.SetActive (false);
+			//Debug.Log ("Player collided with the enemy");
+			//rb2d.gameObject.SetActive (false);
+
+		}
+
+	}
+
+	void OnDestroy() {
+		//SavePlayerPos ();
+	}
+
+
+	public void SavePlayerPos() {
+		PlayerPrefs.SetFloat ("X", transform.position.x);
+		PlayerPrefs.SetFloat ("Y", transform.position.y);
+		PlayerPrefs.SetFloat ("Z", transform.position.z);
+	}
+
+	public void LoadPlayerPos(float xOffset, float yOffset, float zOffset) {
+		//transform.position.x = PlayerPrefs.GetFloat ("X");
+		//transform.position.y = PlayerPrefs.GetFloat ("Y");
+		//transform.position.z = PlayerPrefs.GetFloat ("Z");
+
+		Vector3 playerPos = new Vector3 (PlayerPrefs.GetFloat ("X") + xOffset, PlayerPrefs.GetFloat ("Y") 
+										+ yOffset, PlayerPrefs.GetFloat ("Z") + zOffset);
+		transform.position = playerPos;
+	}
+
+	public void Flip() {
+		Vector3 playerScale = transform.localScale;
+		playerScale.x = playerScale.x * -1;
+		transform.localScale = playerScale;
+		isFacingRight = !isFacingRight;
+	}
+}
---
+++ game/Assets/scripts/Questions.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Questions : MonoBehaviour {
+
+	// Create a 2D string array for questions and answers
+	public static string[,] qa = new string[,] {
+		{
+			"What's the most common cancer?",
+			"Lung Cancer",
+			"Bladder Cancer",
+			"Skin Cancer",
+			"Breast Cancer",
+			"3",
+		},
+
+		{
+			"In the 1990's, about how many women has \ncontracted breast cancer??",
+			"1,100,000",
+			"2,600,000",
+			"3,500,000",
+			"2,000,000",
+			"4",
+		},
+
+		{
+			"Which Cancer is most deadly?",
+			"Kidney Cancer",
+			"Lung Cancer",
+			"Breast Cancer",
+			"Colon Cancer",
+			"2",
+		},
+
+		{
+			"How much money is spent on Cancer??",
+			"20 billion",
+			"45 billion",
+			"100 billion",
+			"75 billion",
+			"3"
+		},
+
+		{
+			"More questions 1?",
+			"a1 1",
+			"a2 1",
+			"a3 1",
+			"a4 1",
+			"1"
+		},
+
+		{
+			"More questions 2?",
+			"a1 2",
+			"a2 2",
+			"a3 2",
+			"a4 2",
+			"2"
+		},
+	};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+
+}
