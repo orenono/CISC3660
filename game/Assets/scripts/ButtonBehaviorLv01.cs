716aa7c57c8658e4241057542c6056162b69e7ef
sggeniussg <jeffreywu0216@gmail.com>
2016-11-08 14:40:39 -0500

    Add the menu scene, and Button for level01 scene, also space will pause or resume the game

---
+++ game/Assets/scripts/ButtonBehaviorLv01.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonBehaviorLv01 : MonoBehaviour {
+
+	public GameObject[] DisplayWhenPaused;
+	public GameObject[] DisplayWhenResumed;
+	public bool paused;
+
+	// Use this for initialization
+	void Start () {
+
+		//if (pauseText == null) {
+		DisplayWhenPaused = GameObject.FindGameObjectsWithTag("Paused");
+		foreach (GameObject dp in DisplayWhenPaused)
+			dp.SetActive (false);
+		//if (pauseText == null) {
+		DisplayWhenResumed = GameObject.FindGameObjectsWithTag("Unpaused");
+		foreach (GameObject dp in DisplayWhenResumed)
+			dp.SetActive (true);
+
+		//pauseText.SetActive(false);
+		paused = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump") && !paused) 
+			PauseGame ();
+		else if (Input.GetButtonDown ("Jump") && paused) 
+			ResumeGame ();
+	}
+
+	public void PauseGame()
+	{
+		paused = true;
+		Time.timeScale = 0.0f;
+		//pauseText.SetActive (true);
+		foreach (GameObject dp in DisplayWhenPaused)
+			dp.SetActive (true);
+		foreach (GameObject dp in DisplayWhenResumed)
+			dp.SetActive (false);
+	}
+
+	public void ResumeGame()
+	{
+		paused = false;
+		Time.timeScale = 1.0f;
+		//pauseText.SetActive (false); 
+		foreach (GameObject dp in DisplayWhenPaused)
+			dp.SetActive (false);
+		foreach (GameObject dp in DisplayWhenResumed)
+			dp.SetActive (true);
+
+
+	}
+
+	public void Menu()
+	{
+		SceneManager.LoadScene ("main");
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
