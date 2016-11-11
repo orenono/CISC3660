d1ebeabf0b152846463c93ba8d9d146c97600ecd
Oren Lahav <orenlahav@yahoo.com>
2016-11-11 00:28:00 -0500

    1) Added a “start menu” scene 2) Added an “Intro” cutscene

---
+++ game/Assets/scripts/MenuScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startButton;
+	public Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startButton = startButton.GetComponent<Button> ();
+		exitButton = exitButton.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPressed()
+	{
+		quitMenu.enabled = true;
+		exitButton.enabled = false;
+		startButton.enabled = false;
+	}
+
+	public void DontQuitPress()
+	{
+		quitMenu.enabled = false;
+		startButton.enabled = true;
+		exitButton.enabled = true;
+
+	}
+		public void StartGame()
+		{
+			SceneManager.LoadScene("IntroCutscene");
+		}
+
+		public void ExitGame()
+		{
+			Application.Quit ();
+		}
+
+
+	}
+	
---
+++ game/Assets/scripts/TextMovingScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextMovingScript : MonoBehaviour {
+
+	private RectTransform textPosition;
+	public float speed = 7.0f;
+
+	// Use this for initialization
+	void Start () {
+		if (textPosition == null)
+		textPosition = GetComponent<RectTransform> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		textPosition.Translate (-1 * speed, 0, 0);
+
+		if ((textPosition.position.x <= -13000) || Input.anyKey == true)
+			LoadGame ();
+	
+	}
+
+	void LoadGame()
+	{
+		SceneManager.LoadScene ("level01");
+	}
+}
