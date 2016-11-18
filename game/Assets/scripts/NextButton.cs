bb04e7ec3d66f39f35af88ede45256bffdc33004
Syed Raza <razasyedm@yahoo.com>
2016-11-17 20:42:53 -0500

    Added Answer highlighting, fixed next button 2

--- game/Assets/scripts/NextButton.cs
+++ game/Assets/scripts/NextButton.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 public class NextButton : MonoBehaviour {
 	public Transform resultObj;
 	private AudioSource nextButtonAudio;
+	QuestionController qc;
 
 	// Use this for initialization
 	void Start () {
-
+		if (qc == null)
+			qc = GameObject.FindObjectOfType (typeof(QuestionController)) as QuestionController; 
+		
 		if (nextButtonAudio == null)
 			nextButtonAudio = GetComponent <AudioSource> ();
 
@@ -16,34 +19,43 @@ public class NextButton : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnMouseDown() {
 		nextButtonAudio.Play ();
-		
-		// Change to another question and
-		// limit the counter within the question array
-		if (QuestionController.counter <= Questions.qa.GetLength (0)) {
-			QuestionController.counter++;
 
-			// When it's the 4th question, change the next button text
-			if (QuestionController.counter % 3 == 0) {
-				GetComponent<TextMesh> ().text = "Finish";
-			}
+		// Only allow the player to click next if they have picked an answer
+		if (AnswerWithMouse.lockAnswer == true) {	
 
-			// Once the player hits 'Finish' button, change the scene
-			if (QuestionController.counter % 4 == 0) {
+			// Change to another question and
+			// limit the counter within the question array
+			if (QuestionController.counter <= Questions.qa.GetLength (0)) {
+				QuestionController.counter++;
 
-				//QuestionController.counter = 0;
-				Debug.Log ("Changing scene to level01");
-				SceneManager.LoadScene ("level01");
+				// When it's the 4th question, change the next button text
+				if (QuestionController.counter % 3 == 0) {
+					GetComponent<TextMesh> ().text = "Finish";
+				}
+
+				// Once the player hits 'Finish' button, change the scene
+				if (QuestionController.counter % 4 == 0) {
+
+					//QuestionController.counter = 0;
+					Debug.Log ("Changing scene to level01");
+					SceneManager.LoadScene ("level01");
+				}
 			}
 		}
 
-
 		AnswerWithMouse.lockAnswer = false;
-		resultObj.GetComponent<TextMesh> ().text = "";	
+
+		// Reset result text, answer text and colors
+		resultObj.GetComponent<TextMesh> ().text = "Pick an answer";	
+		qc.ResetAnswerColors (qc.ans1, Color.white, 36);
+		qc.ResetAnswerColors (qc.ans2, Color.white, 36);
+		qc.ResetAnswerColors (qc.ans3, Color.white, 36);
+		qc.ResetAnswerColors (qc.ans4, Color.white, 36);
 	}
 				
 }
